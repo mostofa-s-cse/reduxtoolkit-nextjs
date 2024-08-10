ea331ce98d6bdde05232a590296e754740e7df47
mostofa-s-cse <mostofa.s.cse@gmail.com>
2024-08-10 10:31:27 +0600

    test-min

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Home from "@/components/Home";
 export default function Page() {
   return (
     <div className="mx-auto px-8 py-9">
+        <h2>Home Page</h2>
        <Home/>
     </div>
   );
